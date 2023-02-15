b5d07f732cb8fefb7df356855f1a4e0511daf9df
kingstockton@gmail.com <>
2023-02-15 15:29:24 -0700

    Add pins reviews login and reiger setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,179 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useCallback } from "react";
+import Map, {
+  Marker,
+  FullscreenControl,
+  NavigationControl,
+  Popup,
+} from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import "./app.css";
+import axios from "axios";
+import { format } from "timeago.js";
+import StarIcon from "@mui/icons-material/Star";
+import RoomIcon from "@mui/icons-material/Room";
+import Register from "./components/Register";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [pins, setPins] = useState([]);
+  const [currentPlaceId, setCurrentPlaceId] = useState(null);
+  const [newPlace, setNewPlace] = useState(null);
+  const [viewState, setViewState] = useState({
+    longitude: -117,
+    latitude: 32.597,
+    zoom: 9,
+  });
+  const [title, setTitle] = useState();
+  const [desc, setDesc] = useState();
+  const [rating, setRating] = useState();
+  const mapRef = React.useRef();
+
+  useEffect(() => {
+    const getPins = async () => {
+      try {
+        const res = await axios.get("/pins");
+        setPins(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getPins();
+  }, []);
+
+  const handleMarkerClick = useCallback((id, lat, lng) => {
+    setCurrentPlaceId(id);
+    setViewState({...viewState, latitude: lat, longitude: lng})
+  },[])
+
+  const handleAddClick = React.useCallback((e) => {
+    const {lng, lat} = e.lngLat;
+    setNewPlace({long: lng, lat});
+  },[]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const newPin = {
+      username: currentUser,
+      title,
+      desc,
+      rating,
+      lat: newPlace.lat,
+      long: newPlace.long,
+    }
+    
+    try {
+      const res = await axios.post("/pins", newPin);
+      setPins([...pins, res.data]);
+      setNewPlace(null);
+    }catch(err) {
+      console.log(err)
+    }
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Map
+      ref={mapRef}
+      mapboxAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+      style={{
+        width: "100vw",
+        height: "100vh",
+      }}
+      {...viewState}
+      doubleClickZoom={false}
+      onMove={(evt) => setViewState(evt.viewState)}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+      onDblClick={(e) => handleAddClick(e)}
+    >
+      {pins?.map((p, index) => (
+        <React.Fragment key={index}>
+          <Marker
+            offset={[0, 0]}
+            longitude={p.long}
+            latitude={p.lat}
+            anchor="top"
+            draggable={true}
+          >
+            <RoomIcon onClick={() => handleMarkerClick(p._id, p.lat, p.long)} />
+          </Marker>
+          {p._id === currentPlaceId && (
+            <Popup
+              latitude={p.lat}
+              longitude={p.long}
+              closeButton={true}
+              closeOnClick={false}
+              onClose={() => setCurrentPlaceId(null)}
+              anchor="left"
+            >
+              <div>
+                <label>Place</label>
+                <h4>{p.title}</h4>
+                <label>Review</label>
+                <p className="desc">{p.desc}</p>
+                <label>Rating</label>
+                <div>
+                  {Array(p.rating).fill(<StarIcon className="star" />)}
+                </div>
+                <label>Information</label>
+                <p className="username">
+                  Created By: <b>{p.username}</b>
+                </p>
+                <span className="date">{format(p.createdAt)}</span>
+              </div>
+            </Popup>
+          )}
+        </React.Fragment>
+      ))}
+      {newPlace && (
+        <Popup
+          latitude={newPlace.lat}
+          longitude={newPlace.long}
+          closeButton={true}
+          closeOnClick={false}
+          anchor="left"
+          onClose={() => setNewPlace(null)}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <div>
+            <form onSubmit={handleSubmit}>
+              <label>Title</label>
+              <input 
+              placeholder="Enter a title"
+              onChange={(e) => setTitle(e.target.value)}
+              />
+              <label>Review</label>
+              <textarea placeholder="Say something about this place"
+              onChange={(e) => setDesc(e.target.value)}
+              />
+              <label>Rating</label>
+              <select               
+              onChange={(e) => setRating(e.target.value)}
+>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+              </select>
+              <button className="submitButton" type="submit">
+                Add Pin
+              </button>
+            </form>
+          </div>
+          </Popup>
+      )}
+      <NavigationControl position="top-right" />
+      <FullscreenControl />
+      {currentUser ? (
+      <button className="button logout">Log Out</button>) 
+        : (
+        <div className='buttons'>
+          <button className="button login">Login</button>
+          <button className="button register">Register</button>
+        </div>)
+        }
+      
+      <Register onSetCurrentUser={setCurrentUser}/>
+    </Map>
   );
 }
 
---
+++ src/components/Register.jsx
@@ -0,0 +1,54 @@
+import './register.css';
+import React, { useState, useRef } from 'react';
+import { Room } from '@mui/icons-material';
+import axios from 'axios';
+
+export default function Register() {
+  const [ success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
+  const nameRef = useRef(null)
+  const emailRef = useRef(null)
+  const passwordRef = useRef(null)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const newUser = {
+      username: nameRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value
+    }
+
+    setSuccess(true);
+
+    try {
+     const res = await axios.post('/users/register', newUser);
+      setSuccess(true)
+      console.log(res)
+      setError(false)
+    } catch(err) {
+      console.log(err)
+      setError(true)
+      setSuccess(false)
+    }
+  }
+  return (
+    <div className='registerContainer'>
+      <div className="logo"> 
+        <Room/> Travel Pin
+      </div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="username" ref={nameRef}/>
+        <input type="email" placeholder="email" ref={emailRef}/>
+        <input type="password" placeholder="password" ref={passwordRef}/>
+        <button className='registerBtn'>Register</button>
+        {success && (
+          <span className='success'> Logged in!</span>
+          )}
+        {error && (
+        <span className='failure'> Opps Sorry</span>
+        )}
+      </form>
+    </div>
+  );
+}
